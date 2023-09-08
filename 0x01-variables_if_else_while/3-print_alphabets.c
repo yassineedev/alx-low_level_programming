0e71842cbeb421414d5fe04f0f02898d32e0e5b1
yassineedev <yassine.boussif18@gmail.com>
2023-09-08 19:34:52 +0100

    dias

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-The main function
+ * Description: This is nothing
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n, g;
+		for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+		for (g = 65; g <= 90; g++)
+		{
+			putchar(g);
+		}
+			putchar(10);
+			return (0);
+}
