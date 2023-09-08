dbb54553b644fb2e66bfbadceba0cd72b0997265
yassineedev <yassine.boussif18@gmail.com>
2023-09-08 20:10:54 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-The main function
+ * Description: This is nothing
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+		for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	return (0);
+}
