8010532ebe393f0a39ec12281d42e7cccf5d6822
yassineedev <yassine.boussif18@gmail.com>
2023-09-08 20:41:22 +0100

    taskk

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-The main function
+ * Description: This is nothing
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+		for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	int b;
+		for (b = 97; b <= 102; b++)
+	{
+		putchar(b);
+	}
+		putchar(10);
+	return (0);
+}
