6ac6f0e866e527ea4ca44c146284cce4440cd585
yassineedev <yassine.boussif18@gmail.com>
2023-09-08 20:58:55 +0100

    taskk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,11 @@ int main(void)
 		for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		putchar(',');
-		putchar(' ');
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 		putchar(10);
 	return (0);
