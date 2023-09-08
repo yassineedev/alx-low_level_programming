29d099e1e234c02c3a6b3faa5c3d54f72f14935c
yassineedev <yassine.boussif18@gmail.com>
2023-09-08 20:26:37 +0100

    code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-The main function
+ * Description: This is nothing
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+		for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+		putchar(10);
+	return (0);
+}
