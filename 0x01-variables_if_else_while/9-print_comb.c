4f042f94655b6ec6a229e4918b9812b6937c3fe6
yassineedev <yassine.boussif18@gmail.com>
2023-09-08 20:53:48 +0100

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-The main function
+ * Description: This is nothing
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+		for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+		putchar(10);
+	return (0);
+}
