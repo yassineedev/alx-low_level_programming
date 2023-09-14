333c83054ef5f94bf648d562063226c4bf524bee
yassineedev <yassine.boussif18@gmail.com>
2023-09-14 23:53:22 +0100

    here we go

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: result return
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
