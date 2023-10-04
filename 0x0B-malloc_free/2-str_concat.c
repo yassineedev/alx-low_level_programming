c287e8ff68e3446f994dc5701dba6c8f5daf592b
yassineedev <yassine.boussif18@gmail.com>
2023-10-04 23:45:36 +0100

    task 3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *str_concat - cs
+ * @s1: string 1
+ * @s2: other string
+ *
+ * Return: pointer to the new string created (Success), or NULL (1)
+ */
+char *str_concat(char *s1, char *s2)
+{
