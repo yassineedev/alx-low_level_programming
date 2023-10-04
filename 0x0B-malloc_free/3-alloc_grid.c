cdef9eb82634ec638487d517b7b8cf083d49ffa8
yassineedev <yassine.boussif18@gmail.com>
2023-10-04 23:46:58 +0100

    task 2

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - ...
+ * @width: wm
+ * @height: hm
+ *
+ * Return: pointer to the created matrix (Success)
+ * or NULL (1)
+ */
+int **alloc_grid(int width, int height)
