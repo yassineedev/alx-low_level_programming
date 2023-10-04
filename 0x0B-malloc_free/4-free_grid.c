f1c1e41d915b772ad5398eeb317cff68fc138080
yassineedev <yassine.boussif18@gmail.com>
2023-10-04 23:48:17 +0100

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - fmag
+ * alloc_grid()
+ * @grid: grid 2 free
+ * @height: height 2 the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
