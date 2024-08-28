dc5b8a4c5ae39cf9340fbba0e82c2e332ca3fd55
yassineedev <yassine.boussif18@gmail.com>
2024-08-28 12:39:56 +0100

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - main function
+* Return: this time 1
+*/
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
+
