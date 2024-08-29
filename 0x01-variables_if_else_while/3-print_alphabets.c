f3568b59971cd935e1abeed446e279cb76671732
yassineedev <yassine.boussif18@gmail.com>
2024-08-29 01:30:57 +0100

    test for task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main is main
+* des for my code
+* Return: always 0 success
+*/
+int main (void)
+{
+	int f, s;
+		for (f = 97; f <= 122 ; f++)
+	{
+		putchar(f);
+	}
+		for (s = 65; s <= 90 ; s++)
+		{
+			putchar(s);
+		}
+			putchar('\n');
+			return (0);
+}
