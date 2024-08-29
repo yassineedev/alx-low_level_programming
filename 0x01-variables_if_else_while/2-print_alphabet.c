3312ca3d6b504eb83b07e447a6ca539e9cd30553
yassineedev <yassine.boussif18@gmail.com>
2024-08-29 16:06:06 +0100

    task number 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main is main
+* des of my code
+* Return: always 0
+*/
+int main(void)
+{
+	int c;
+		for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+		return (0);
+}
