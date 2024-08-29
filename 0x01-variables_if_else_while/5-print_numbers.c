3f579a943fb0bda800127ef091a7560818e31e7a
yassineedev <yassine.boussif18@gmail.com>
2024-08-29 16:48:12 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main is main
+ * des of my function
+ * Return: always )
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
