dc136fc98923546bbf3e1e765cd4d03351bc4328
yassineedev <yassine.boussif18@gmail.com>
2024-08-29 16:58:02 +0100

    task number 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main is main
+ * des of my code
+ * Return: always )
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+		putchar('\n');
+		return (0);
+}
