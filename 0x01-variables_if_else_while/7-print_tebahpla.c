ec5cbe7c100febf9f9087f3656398106c1b67342
yassineedev <yassine.boussif18@gmail.com>
2024-08-29 17:13:36 +0100

    task number 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main is main
+ * des of my code
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
