0010640a5535f6fe75dfa6ce1ad3a49287ade886
yassineedev <yassine.boussif18@gmail.com>
2024-08-31 13:38:05 +0100

    task number 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - prints lower cases
+ *  Return: Return 1 if is lower case and return 0 if it it's not
+ */
+int _islower(int c)
+{
+	for (c = 97; c <=122; c++)
+		return (1);
+	return (0);
+}
