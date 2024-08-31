7ccc974a1ca1b3d552b393e77024fc38134e26cb
yassineedev <yassine.boussif18@gmail.com>
2024-08-31 13:47:57 +0100

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
  * _islower - prints lower cases
- *  Return: Return 1 if is lower case and return 0 if it it's not
+ *
+ * 0c: means check the inputs
+ *
+ *  Return: Return 1 if 'c' is lower case and return 0 if it it's not
  */
 int _islower(int c)
 {
-	for (c = 97; c <=122; c++)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
