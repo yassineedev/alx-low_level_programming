4f1889af2cd9c08f6a0ea6e333900a887a022740
yassineedev <yassine.boussif18@gmail.com>
2024-09-01 17:53:03 +0100

    task 6 absulet value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computed function that give us absulet value
+ * @n: an int
+ * Return: return n if is great than 0 & return -n if is less than 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
