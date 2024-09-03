d4750423c2c3d1aa6aa305cc1aead5dce3ffbbec
yassineedev <yassine.boussif18@gmail.com>
2024-09-03 17:50:26 +0100

    task number 2 multi function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - is digit
+ * @c: is int
+ * Return: always 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - mul is mul
+ * @a: is int
+ * @b: is int
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
