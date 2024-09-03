69b3a904f59048ecff92c713e4c7d88de8d111c1
yassineedev <yassine.boussif18@gmail.com>
2024-09-03 18:06:29 +0100

    task number 3 - putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,11 @@
  */
 void print_numbers(void)
 {
-	int c;
+	int n;
 
-	for (c >= '0'; c <= '9'; c++)
+	for (n = 0; n < 10; n++)
 	{
-		_putchar(c);
+		_putchar(n + '0');
 	}
 		_putchar('\n');
-		return (0);
 }
