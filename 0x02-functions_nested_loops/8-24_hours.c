51d83dbe50675dc82f37dc50c46b228323c6432d
yassineedev <yassine.boussif18@gmail.com>
2024-09-03 12:50:24 +0100

    test

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - print m an hours of jack bauer's day
+ * starting from 00:00
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 0;
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
