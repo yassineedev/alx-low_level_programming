7cc08788adce80efa6fd3674aac0c0ec84757dfd
yassineedev <yassine.boussif18@gmail.com>
2024-09-04 20:02:53 +0100

    task number 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,3 +12,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void more_numbers(void);
+void print_most_numbers(void);
