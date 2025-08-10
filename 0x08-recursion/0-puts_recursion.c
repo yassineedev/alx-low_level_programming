af07d5a990e803576de7cd16e3de69a02c5c79e2
yassineedev <yassine.boussif18@gmail.com>
2025-08-10 15:51:30 +0100

    put_str

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursive - Prints a string followed by a new line
+ * @s: Pointer to the string
+ *
+ * This function prints characters one by one recursively until it
+ * reaches the null terminator, then it prints a newline.
+ */
+
+void	_puts_recursive(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+		_putchar(*s);
+	return (_puts_recursive(s + 1));
+}
