5582861534df74b58ab87e18d313adfab099f440
yassineedev <yassine.boussif18@gmail.com>
2025-08-11 13:50:34 +0100

    task 01 des

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
-/*
- * _print_rev_recursion - to print in rev
- * s - is pointer to string
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: Pointer to the string to be printed.
+ *
+ * Description: This function recursively moves to the end of the string
+ * and prints each character on the way back, resulting in reversed output.
  */
 
 void	_print_rev_recursion(char *s)
