af87de57ab6a0743adbc11049e78af8e67c34da3
yassineedev <yassine.boussif18@gmail.com>
2025-08-12 12:31:19 +0100

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _strlen - clacul strlen
+ * @s: is a string
+ *
+ * Return: the index - 1
+ */
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i - 1);
+}
+/**
+ * _palindrome - palindrome rec helper
+ * @s: is a string
+ * @n: index 0
+ * @len: len of ower string
+ *
+ * Return: Call back our function
+ */
+
+int	_palindrome(char *s, int n, int len)
+{
+	if (s[n] != s[len])
+		return (0);
+	if (n >= len)
+		return (1);
+	n++;
+	len--;
+	return (_palindrome(s, n, len));
+}
+
+/**
+ * is_palindrome - get strlen from strlen - 1
+ * @s: is a string
+ *
+ * Return: to the palindrome helper
+ */
+
+int	is_palindrome(char *s)
+{
+	int	len = _strlen(s);
+
+	if (*s == '\0')
+		return (1);
+	return (_palindrome(s, 0, len));
+}
